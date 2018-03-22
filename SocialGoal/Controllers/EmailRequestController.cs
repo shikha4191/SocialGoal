09e8f3e0a7ba47af4924019f23f1c86bdd4b458d
Venkataramana <Ranjitha.Venkataramana@bsci.com>
2018-03-22 16:29:10 +0530

    Change in email request controller.

--- SocialGoal/Controllers/EmailRequestController.cs
+++ SocialGoal/Controllers/EmailRequestController.cs
@@ -31,7 +31,7 @@ namespace SocialGoal.Web.Controllers
 
         public ActionResult AddGroupUser()
         {
-            //
+            var x = 5;
             Guid groupIdToken = (Guid)TempData["grToken"];
             var groupId = securityTokenService.GetActualId(groupIdToken);
             GroupUser newGroupUser = new GroupUser()
